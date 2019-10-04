a158ab92e30be2ea3102ea996d12b77648cef0d8
Carsten Kuckuk <ck@kuckuk.com>
2019-10-04 22:56:18 +0200

    Unittests fuer Funktion DistanceInMeters (#3)
    Wir haben ein paar erste Unittests

---
+++ src/tests/TestsMain.cpp
@@ -0,0 +1,14 @@
+// Datei TestsMain.cpp
+//
+// Einsprungpunkt. Hauptprogramm.
+
+#include "gtest/gtest.h"
+
+int main(int argc, char **argv)
+{
+  ::testing::InitGoogleTest(&argc, argv);
+
+  int nErg = RUN_ALL_TESTS();
+  
+  return nErg;
+}
