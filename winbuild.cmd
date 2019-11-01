a092dcb4bd40f7d1e2c38b98696a6223bce68249
Carsten Kuckuk <ck@kuckuk.com>
2019-11-02 00:55:28 +0100

    Windows build: Switch to VS2019

--- winbuild.cmd
+++ winbuild.cmd
@@ -3,8 +3,9 @@ mkdir xxx
 cd xxx
 REM For Visual Studio 2010 use: cmake -G "Visual Studio 10 Win64" ..
 REM For Visual Studio 2012 use: cmake -G "Visual Studio 11 Win64" ..
-REM For Visual Studio 2017 use:
-cmake -G "Visual Studio 15 2017 Win64" ..
+REM For Visual Studio 2017 use: cmake -G "Visual Studio 15 2017 Win64" ..
+REM For Visual Studio 2019 use: 
+cmake -G "Visual Studio 16 2019" -A x64 ..
 
 cmake --build . --config Release
 
