2ea4368777389e1b3ce692d6c3223cff58e4f416
Carsten Kuckuk <ck@kuckuk.com>
2020-10-25 21:55:54 +0100

    Tool svgfromstackofsvgs kann nun mit beliebig vielen Dateien umgehen (#24)
    * svgfromstackofsvgs: Kann nun mit beliebiger Anzahl an Eingabedateien zurecht kommen

--- winrun.cmd
+++ winrun.cmd
@@ -1,11 +1,12 @@
 mkdir wrk
 cd wrk
 copy ..\data\cities.trackfile .
+copy ..\data\background.svg .
 copy c:\data\backup.sqlite .
 ..\xxx\Release\wiglewifitotrackfile.exe backup.sqlite backup.trackfile
 ..\xxx\Release\svgfromtrackfile.exe backup.trackfile backup.svg
 ..\xxx\Release\svgfromlabellist.exe cities.trackfile cities.svg
-..\xxx\Release\svgfromstackofsvgs.exe map.svg backup.svg cities.svg
+..\xxx\Release\svgfromstackofsvgs.exe map.svg background.svg backup.svg cities.svg
 
 
 cd ..
