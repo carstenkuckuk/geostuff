94adab396db2ebd257c32de2fc3bd7b48414cedf
Carsten Kuckuk <ck@kuckuk.com>
2022-10-31 23:42:26 +0100

    Einfache Erkennung: v<6km/h, 15 Minuten vor Ort, nicht mehr als 50m weg bewegen (#35)

--- src/RestingplaceFinder.hpp
+++ src/RestingplaceFinder.hpp
@@ -24,6 +24,29 @@ protected:
 //	__int64 m_nwwtmTo;   // WigleWife Date Time Format: time_t, aber mit 1000 multipliziert....
 };
 
+class Hypothese
+{
+public:
+	Hypothese();
+	bool m_bGenugZeitZusammenFuerAufenthaltsort;
+
+	__int64 m_nwwtmStartzeit;
+	double m_dStartLat;
+	double m_dStartLong;
+
+	__int64 m_nwwtmLetztezeit;
+	double m_dLetzteLat;
+	double m_dLetzteLong;
+
+	bool ZeitsprungZuGross(__int64 nwwtm); // true, wenn mehr als zwei Minuten zwischen den Observationen vergangen sind
+	bool GeschwindigkeitZuGross(double dLat, double dLong, __int64 nwwtm); // true, wenn man sich schneller als 6 km/h bewegt hat
+	bool DistanzZuGross(double dLat, double dLong); // true, wenn man sich mehr als 50m vom Ort wegbewegt hat
+	bool ZeitAusreichendFuerAufenthaltsort(__int64 nwwtm); // true, wenn wir mindestens 15 Minuten zusammen haben
+
+	void SetzeNeuenLetztenPunkt(double dLat, double dLong, __int64 nwwtm); // Aufrufen, wenn ein neuer Punkt zur Hypothese paﬂt
+	void BeginneMitNeuerHypotheseAb(double dLat, double dLong, __int64 nwwtm);
+};
+
 class RestingplaceFinder
 {
 public:
@@ -34,6 +57,8 @@ public:
 	std::vector<Restingplace> GetRestingplaces();
 protected:
 	std::vector<Restingplace> m_rgRestingplaces;
+
+	Hypothese m_h;
 };
 
 #endif
